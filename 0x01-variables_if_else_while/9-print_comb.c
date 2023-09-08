9dca249d9baa261a92f3f37c16e862724c4aa1aa
Jimmy <jcnyongesa@kabarak.ac.ke>
2023-09-09 00:22:30 +0300

    Patience, persistence and perspiration make an unbeatable combination for succes

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+/**
+ * main - main function
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int j;
+
+	for (j = 0 ; j < 10 ; j++)
+	{
+		if (j == 9)
+		{
+			putchar(j + '0');
+		}
+		else
+		{
+			putchar(j + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
