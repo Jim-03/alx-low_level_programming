1ed268db4d03ce091a84cd80a13f5fe9485cf4bb
Jimmy <jcnyongesa@kabarak.ac.ke>
2023-09-12 22:44:57 +0300

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *print_sign - checks the sign
+ *@n: varibale to be checked
+ *Return: either 1, 0 or -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(45);
+		return (0);
+	}
+	else
+	{
+		_putchar(48);
+		return (-1);
+	}
+
+}
