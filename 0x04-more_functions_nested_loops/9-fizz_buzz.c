cdf3b62f7844bcbb8160b21a3598ff9f2b95d156
Jimmy <jcnyongesa@kabarak.ac.ke>
2023-09-15 01:24:23 +0300

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+
+/**
+ * main - the main function
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i =1 ; i <= 100 ; i++)
+	{
+		int main(void)
+{
+	int i;
+
+	for (i =1 ; i <= 100 ; i++)
+	{
+		if
+		{
+			if(((i / 3) == 0) && (i / 5) == 0)
+	                {
+	                        _putchar("FizzBuzz");
+				_putchar(' ');        	        }
+
+			else if ((i / 3) == 0)
+			{
+				_putchar("Fizz");
+				_putchar(' ');
+			}
+			else if ((i / 5) == 0)
+			{
+				_putchar("Buzz");
+				_putchar(' ');
+			}
+		}
+		else
+		{
+			if (i > 9)
+			{
+				_putchar(i / 10 + '0');
+			}
+			_putchar(i % 10 + '0');
+		}
+		if(i < 100)
+		{
+			_putchar(' ');
+		}
+		else
+			_putchar('\n');
+	}
+	return (0);
+}
