7d0d9736ff75c979190602497cbcb0c9fadc6736
Jim-03 <jcnyongesa@kabarak.ac.ke>
2023-09-21 12:03:52 +0300

    Prints integer with putchar

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_number - prints integers
+ * @n: the integer to be printed
+ */
+
+void print_number(int n)
+{
+	int rem, j, f;
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n == 0)
+		_putchar('0');
+
+	j = 1000;
+	f = 0;
+
+	while (j >= 1)
+	{
+		rem = n / j;
+
+		if (rem > 0 || f || j == 1)
+		{
+			_putchar(rem + '0');
+			f = 1;
+		}
+		n %= j;
+		j /= 10;
+	}
+}
