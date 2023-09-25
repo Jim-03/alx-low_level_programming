19cbb1992f94eb65c7f4387bd36f96639871c611
Jim-03 <jcnyongesa@kabarak.ac.ke>
2023-09-25 15:39:16 +0300

    Adding two diagonals

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * print_diagsums - adds diagonals of square matrix
+ * @a: the square matrix
+ * @size: the size of the square matrix
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int n = size - 1;
+	int sum = 0;
+	int sumA = 0;
+
+	for (i = 0 ; i < size ; i++)
+	{
+		sum += a[i * size + i];
+	}
+
+	for (i = n ; i >= 0 ; i--)
+		sumA += a[i * size - n - i];
+
+	printf("%d, %d \n", sum, sumA);
+}
