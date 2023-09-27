cbb9315e21e9fa09f4ee32907e1c82c0ace38e49
Jim-03 <jcnyongesa@kabarak.ac.ke>
2023-09-27 11:47:42 +0300

    Factorial using recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -10,7 +10,11 @@ int factorial(int n)
 {
 	int fact = n;
 
-	if (n != 0)
+	if (n > 0)
 		fact = fact * factorial(n - 1);
-	return (n);
+	else if (n == 0)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	return (fact);
 }
