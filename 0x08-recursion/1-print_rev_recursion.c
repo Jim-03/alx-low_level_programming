855d6928f6bf5c7015322b358d22d5a0394cd63b
Jim-03 <jcnyongesa@kabarak.ac.ke>
2023-09-27 10:46:07 +0300

    Recursion string reversal

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print string in reversal order
+ * @s: the string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		s++;
+		return;
+	}
+	_putchar(*s);
+	_print_rev_recursion(s + 1);
+	
+	_putchar('\n');
+}
