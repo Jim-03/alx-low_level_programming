a62d9d510ad48234fb1cf2ed5ea3acc2af14725e
Jim-03 <jcnyongesa@kabarak.ac.ke>
2023-10-19 22:43:42 +0300

    Prints before main function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,8 @@ void premain(void) __attribute__((constructor));
  * premain - prints before main runs
  */
 
-void premain(void) __attribute__((constructor))
+void premain(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
