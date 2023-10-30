9946adb022f62c2734640fc8824508dfc2e70212
Jim-03 <jcnyongesa@kabarak.ac.ke>
2023-10-30 21:56:54 +0300

    3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define BUF_SIZE 1024
+
+/**
+ * main - the main function
+ * @argc: arguments
+ * @argv: array of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
+		exit(97);
+	}
+}
