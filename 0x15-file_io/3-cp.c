cb8018956dfb3a7e1df017e5906018c9aa760418
Jim-03 <jcnyongesa@kabarak.ac.ke>
2023-11-03 23:20:44 +0300

    copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -16,9 +16,48 @@
 
 int main(int argc, char *argv[])
 {
+	const char *from = argv[1];
+	int source = open(from, 0_RDONLY);
+	const char *to = argv[2];
+	int destination = open(to, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	char buf[1024];
+	ssize_t bytes;
+
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
+
+	if (source == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", from);
+		exit(98);
+	}
+
+	if (destination == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to);
+		close(source);
+		exit(99);
+	}
+
+	while ((bytes = read(source, buf, sizeof(buf))) > 0)
+	{
+		if (write(destination, buf, bytes) == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", to);
+			close(source);
+			close(destination);
+			exit(99);
+		}
+	}
+
+	if (close(source) == -1 || close(destination) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", errno);
+		exit(100);
+	}
+
+	return (0);
 }
