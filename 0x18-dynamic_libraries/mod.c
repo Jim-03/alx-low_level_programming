9a02c73b34bd402e951776baaefe7b29fd0fb6ea
Jim-03 <jcnyongesa@kabarak.ac.ke>
2023-12-11 10:58:22 +0300

    using c code in python

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mod - finds the modulus of two numbers
+ * @a: first value
+ * @b: second value
+ * Return: the result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * sub - subtracts two values
+ * @a: first value
+ * @b: second value
+ * Return: the result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
